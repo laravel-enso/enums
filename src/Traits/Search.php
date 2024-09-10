5064686b240239c78a9914cee32d050ab79880b1
mishu <mishu@xtelecom.ro>
2024-09-10 15:27:44 +0300

    fixes search

--- src/Traits/Search.php
+++ src/Traits/Search.php
@@ -3,18 +3,12 @@
 namespace LaravelEnso\Enums\Traits;
 
 use Illuminate\Support\Collection;
-use LaravelEnso\Enums\Contracts\Mappable;
-use ReflectionEnum;
 
 trait Search
 {
-    public static function search(mixed $label): self
+    public static function search(string $name): self | null
     {
-        $matches = fn ($enum) => (new ReflectionEnum(self::class))
-            ->implementsInterface(Mappable::class)
-            ? $enum->map() === $label
-            : $enum->value === $label;
-
-        return Collection::wrap(self::cases())->firstWhere($matches);
+        return Collection::wrap(self::cases())
+            ->firstWhere(fn ($enum) => $enum->name === $name);
     }
 }
