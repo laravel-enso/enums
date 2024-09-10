4bc3b761101f8de4b12012da5b350a15a0ad1695
mishu <mishu@xtelecom.ro>
2024-09-10 14:03:39 +0300

    improves search

--- src/Traits/Search.php
+++ src/Traits/Search.php
@@ -3,12 +3,18 @@
 namespace LaravelEnso\Enums\Traits;
 
 use Illuminate\Support\Collection;
+use LaravelEnso\Enums\Contracts\Mappable;
+use ReflectionEnum;
 
 trait Search
 {
-    public static function search($value)
+    public static function search(mixed $label): self
     {
-        return Collection::wrap(self::cases())
-            ->firstWhere(fn ($enum) => $enum->name === $value);
+        $matches = fn ($enum) => (new ReflectionEnum(self::class))
+            ->implementsInterface(Mappable::class)
+            ? $enum->map() === $label
+            : $enum->value === $label;
+
+        return Collection::wrap(self::cases())->firstWhere($matches);
     }
 }
